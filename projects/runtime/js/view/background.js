0e9b718777f4765de0ef678c1b7ee3746b61459b
noahraimondi <ndraimondi326@gmail.com>
2022-02-24 16:05:12 +0000

    runtime

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -26,8 +26,8 @@ var background = function (window) {
         var background;
         
         // ANIMATION VARIABLES HERE:
-        
-     
+        var tree;
+        var buildings = [];
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -36,17 +36,42 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
-            background.addChild(backgroundFill);
+            var backgroundFill = draw.rect(canvasWidth,groundY,'#87969B'); //cREATES A VARIABLE CALLED BCKGROUND FILL a solid color that installs a rectangle that acts as the background
+            background.addChild(backgroundFill); //Add the background to the canvas
             
             // TODO: 3 - Add a moon and starfield
             
-            
+            for (var i = 0; i <= 125; i++) { //The loops function for the code
+            var circle = draw.circle(1,'red','orange',2);
+            circle.x = canvasWidth*Math.random(); // give random x
+            circle.y = groundY*Math.random();// give random y
+            background.addChild(circle);  //Add circles to background
+            }          
+
+            var moon = draw.bitmap('img/moon.png');
+            moon.x = canvasWidth -200;// holds the x value
+            moon.y = groundY -435;// holds the x value
+            moon.scaleX = 0.1125;///Change the x scale of the moon
+            moon.scaleY = 0.1125;// change the y scale of the moon
+            background.addChild(moon); // adds moon to background
+
+
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+            for(var i = 0; i < 5; i++) {
+                var buildingHeight = 300; // Declaired variable, buliding, holds each height
+                var building = draw.rect(75,buildingHeight,'LightGray','Black',1); // deeclares variable to store buliding
+                building.x = 200*i; // Adds 2000 pixels to x value to the buliding each time the game is run
+                building.y = groundY-buildingHeight;//sets the building y posituion by subtract the height of the buliding from ground y
+                background.addChild(building);// Adds the building to the background
+                buildings.push(building);//pushing informstion to the buliding array and store it asan index and store it as an inde
+            }
             
             // TODO 4: Part 1 - Add a tree
-            
+            tree = draw.bitmap('img/tree.png');
+            tree.x = canvasWidth - 500;
+            tree.y = groundY - 825;
+            tree.scaleY = 5
+            background.addChild(tree);
             
         } // end of render function - DO NOT DELETE
         
@@ -60,7 +85,11 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 4: Part 2 - Move the tree!
-            
+            tree.x = tree.x - 50; // Takes current of value x and take 1 pixel 60/second to move the tree to the left
+            // if the tree is less than -200 pixels then reasign canvasWidth to the trees x position
+            if(tree.x < -200) {//This is what makes the tree  move from thhe left side to the right side 
+                tree.x = canvasWidth;
+            }
             
             // TODO 5: Part 2 - Parallax
             
