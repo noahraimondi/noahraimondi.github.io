4782a00bfef9205268ef51465561120e2838a4d1
noahraimondi <ndraimondi326@gmail.com>
2022-11-16 19:46:49 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -29,12 +29,16 @@ function runProgram(){
     positionY: 0,
     speedX: 0,
     speedY: 0,
+    width: $("#walker").width(),
+    height: $("#walker").height(),
   }
   var agatha = {
     positionX: 0,
     positionY: 0,
     speedX: 0,
     speedY: 0,
+    width: $("#agatha").width(),
+    height: $("#width").height(),
   }
 
   // Game Item Objects
@@ -56,6 +60,7 @@ function runProgram(){
     repositionGameItem();
     redrawGameItem ();
     limitPosition ();
+    //ifTouch(walker, agatha)
   }
   
   /* 
@@ -126,97 +131,162 @@ function runProgram(){
     
     
     /* Might go back to this later in the year...
-    //function noTouch (){
-    
-   if (walker.positionX +25 === agatha.positionX || walker.positionY +25 === agatha.positionY){
-      if (walker.speedX === -5){
-        walker.positionX += 5; 
-      } else if (walker.speedX === 5){
-        walker.positionX -= 5;
-      } else if (agatha.speedX === -5){
-        agatha.positionX += 5; 
-      } else if (agatha.speedX === 5){
-        agatha.positionX -= 5;
-      } else if (walker.speedY === -5){
-        walker.positionY += 5; 
-      } else if (walker.speedY === 5){
-        walker.positionY -= 5;
-      } else if (agatha.speedY === -5){
-        agatha.positionY += 5; 
-      } else if (agatha.speedY === 5){
-        agatha.positionY -= 5;
+    var BODY1_HEIGHT = $("#walker").height();=
+    var BODY1_WIDTH = $("#agatha").height();
+
+    var BODY2_HEIGHT = $("#walker").height();
+    var BODY2_WIDTH = $("#agatha").height();
+
+
+    //function ifTouch (body1, body2){
+      //sides of the walker
+      body1.leftX = body1.x;
+      body1.topY = body1.y;
+      body1.rightX = body1.x + BODY1_WIDTH;
+      body1.bottomY = body1.y + BODY1_HEIGHT;
+
+      //sides of the agatha
+      body2.leftX = body2.x;
+      body2.topY = body2.y;
+      body2.rightX = body2.x + BODY2_WIDTH;
+      body2.bottomY = body2.y + BODY2_HEIGHT;
+
+      if (body2.rightX > body1.leftX &&
+        body2.leftX < body1.rightX &&
+        body2.bottomY > body1.topY &&
+        body2.topY < body1.bottomY){
+        walker = -walker;
+        walker = -walker;
+        agatha = -agatha;
+        agatha = -agatha;
+        return true;
+      } else {
+        return false;
+      }        
+    }
+
+
+
+
+
+
+
+        if (walker.positionX +25 === agatha.positionX || walker.positionY +25 === agatha.positionY){
+            if (walker.speedX === -5){
+              walker.positionX += 5; 
+            } else if (walker.speedX === 5){
+              walker.positionX -= 5;
+            } else if (agatha.speedX === -5){
+              agatha.positionX += 5; 
+            } else if (agatha.speedX === 5){
+              agatha.positionX -= 5;
+            } else if (walker.speedY === -5){
+              walker.positionY += 5; 
+            } else if (walker.speedY === 5){
+              walker.positionY -= 5;
+            } else if (agatha.speedY === -5){
+              agatha.positionY += 5; 
+            } else if (agatha.speedY === 5){
+              agatha.positionY -= 5;
+            }
+          }
+        }
+   */
+
+
+
+    function ifTouch (body1, body2){
+      //sides of the walker
+      body1.leftX = body1.x;
+      body1.topY = body1.y;
+      body1.rightX = body1.x + BODY1_WIDTH;
+      body1.bottomY = body1.y + BODY1_HEIGHT;
+
+      //sides of the agatha
+      body2.leftX = body2.x;
+      body2.topY = body2.y;
+      body2.rightX = body2.x + BODY2_WIDTH;
+      body2.bottomY = body2.y + BODY2_HEIGHT;
+
+      if (body2.rightX > body1.leftX &&
+        body2.leftX < body1.rightX &&
+        body2.bottomY > body1.topY &&
+        body2.topY < body1.bottomY){
+        walker = -walker;
+        walker = -walker;
+        agatha = -agatha;
+        agatha = -agatha;
+        return true;
+      } else {
+        return false;
+      }        
+    }
+
+
+
+    // Only of walker
+    function respondKeyDownW (event){
+      if(event.which === KEYW.DOWN){
+        console.log("Down pressed"); 
+        walker.speedY = +5;
+      } else if(event.which === KEYW.LEFT){
+        walker.speedX = -5;
+        console.log("Left pressed");
+      } else if(event.which === KEYW.RIGHT){
+        walker.speedX = +5;
+        console.log("Right pressed");
+      } else if(event.which === KEYW.UP){
+        walker.speedY = -5;
+        console.log("Up pressed");
       }
     }
-  }
-  */
 
+    function respondKeyUpW (event){
+      if(event.which === KEYW.DOWN){
+        console.log("Down released"); 
+        walker.speedY = 0;
+      } else if(event.which === KEYW.LEFT){
+        walker.speedX = 0;
+        console.log("Left released");
+      } else if(event.which === KEYW.RIGHT){
+        walker.speedX = 0;
+        console.log("Right released");
+      } else if(event.which === KEYW.UP){
+        walker.speedY = 0;
+        console.log("Up released");
+      }
+    }
 
+    // Only for agatha
+    function respondKeyDownA (event){
+      if(event.which === KEYA.DOWN){
+        console.log("Down pressed"); 
+        agatha.speedY = +5;
+      } else if(event.which === KEYA.LEFT){
+        agatha.speedX = -5;
+        console.log("Left pressed");
+      } else if(event.which === KEYA.RIGHT){
+        agatha.speedX = +5;
+        console.log("Right pressed");
+      } else if(event.which === KEYA.UP){
+        agatha.speedY = -5;
+        console.log("Up pressed");
+      }
+    }
 
-
-
-// Only of walker
-function respondKeyDownW (event){
-  if(event.which === KEYW.DOWN){
-    console.log("Down pressed"); 
-    walker.speedY = +5;
-  } else if(event.which === KEYW.LEFT){
-    walker.speedX = -5;
-    console.log("Left pressed");
-  } else if(event.which === KEYW.RIGHT){
-    walker.speedX = +5;
-    console.log("Right pressed");
-  } else if(event.which === KEYW.UP){
-    walker.speedY = -5;
-    console.log("Up pressed");
-  }
-}
-
-function respondKeyUpW (event){
-  if(event.which === KEYW.DOWN){
-    console.log("Down released"); 
-    walker.speedY = 0;
-  } else if(event.which === KEYW.LEFT){
-    walker.speedX = 0;
-    console.log("Left released");
-  } else if(event.which === KEYW.RIGHT){
-    walker.speedX = 0;
-    console.log("Right released");
-  } else if(event.which === KEYW.UP){
-    walker.speedY = 0;
-    console.log("Up released");
-  }
-}
-
-// Only for agatha
-function respondKeyDownA (event){
-  if(event.which === KEYA.DOWN){
-    console.log("Down pressed"); 
-    agatha.speedY = +5;
-  } else if(event.which === KEYA.LEFT){
-    agatha.speedX = -5;
-    console.log("Left pressed");
-  } else if(event.which === KEYA.RIGHT){
-    agatha.speedX = +5;
-    console.log("Right pressed");
-  } else if(event.which === KEYA.UP){
-    agatha.speedY = -5;
-    console.log("Up pressed");
-  }
-}
-
-function respondKeyUpA (event){
-  if(event.which === KEYA.DOWN){
-    console.log("Down released"); 
-    agatha.speedY = 0;
-  } else if(event.which === KEYA.LEFT){
-    agatha.speedX = 0;
-    console.log("Left released");
-  } else if(event.which === KEYA.RIGHT){
-    agatha.speedX = 0;
-    console.log("Right released");
-  } else if(event.which === KEYA.UP){
-    agatha.speedY = 0;
-    console.log("Up released");
-  }
-}
+    function respondKeyUpA (event){
+      if(event.which === KEYA.DOWN){
+        console.log("Down released"); 
+        agatha.speedY = 0;
+      } else if(event.which === KEYA.LEFT){
+        agatha.speedX = 0;
+        console.log("Left released");
+      } else if(event.which === KEYA.RIGHT){
+        agatha.speedX = 0;
+        console.log("Right released");
+      } else if(event.which === KEYA.UP){
+        agatha.speedY = 0;
+        console.log("Up released");
+      }
+    }
 }
