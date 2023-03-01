7872e9107eddfaef51c63e3252dd23955988ef8e
noahraimondi <ndraimondi326@gmail.com>
2023-03-01 18:20:17 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -16,6 +16,7 @@ function runProgram(){
     S: 83,
     C: 38,
     F: 40,
+    S: 32,
   }
 
   const BOARDWIDTH = $("#board").width()
@@ -36,7 +37,7 @@ function runProgram(){
 
   var paddleL = GameItem(37, 200, 0, 0, "#paddle1")
   var paddleR = GameItem(BOARDWIDTH -$("#paddle2").width() -37, 200, 0, 0, "#paddle2")
-  var ball = GameItem(BOARDWIDTH/2, BOARDHEIGHT/2, (Math.random() > 0.5 ? -5: 5), (Math.random() > 0.5 ? -5: 5), "#ball" )
+  var ball = GameItem(BOARDWIDTH/2, BOARDHEIGHT/2, 0, 0, "#ball" )
 
 
 
@@ -47,12 +48,7 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-  
-  PaddleBorder(paddleL, ball)// Sets up the paddle as a solid object for the ball to bounce off of
-  PaddleBorder(paddleR, ball)
 
-  SetYBorder(ball)
-  
   /* 
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
@@ -65,6 +61,13 @@ function runProgram(){
     updateItem(paddleL)
     updateItem(paddleR)
     updateItem(ball)
+
+    PaddleBorder(paddleL, ball)// Sets up the paddle as a solid object for the ball to bounce off of
+    PaddleBorder(paddleR, ball)
+
+    ballBorder(ball)
+    //SetYBorder(paddleL)
+    //SetYBorder(paddleR)
   }
   
   /* 
@@ -87,6 +90,7 @@ function runProgram(){
   function updateItem (itm){//Updates current position of the inputted factor
     itm.xPos = itm.xPos + itm.speX
     itm.yPos = itm.yPos + itm.speY
+    
   }
 
   function drawItem(itm){ //Displays current position of the inputted factor
@@ -98,6 +102,22 @@ function runProgram(){
     (event.which === key1) ? spe1.speY += speed: (event.which === key2) ? spe1.speY -= speed: spe1.speY = 0;
   }
 
+  function ballStart (event, key, speed){
+    (event.which === key1) ? spe1.speY += speed: (event.which === key2) ? spe1.speY -= speed: spe1.speY = 0;
+  }
+
+  function PaddleBorder (key1, key2){ //Reverses the speed of x when x hits y
+  }
+
+  function ballBorder (key){
+    if (key.yPos == 0) {
+      key.speY = -key.speY
+    }
+    if (key.yPos == BOARDHEIGHT) {
+      key.speY = -key.speY
+    }
+  }
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -105,61 +125,8 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-
-  /*
-  function limitPosition (){
-    if (walker.positionX +50 >= $("#board").width()){
-      walker.positionX = $("#board").width()-$("#walker").left();
-    } else if (walker.positionX <= 0){
-      walker.positionX = 0;
-    }
-    if (walker.positionY +50 >= $("#board").height()){
-      walker.positionY = $("#board").height()-50;
-    } else if (walker.positionY <= 0){
-      walker.positionY = 0;
-    }
-  }
-  */
-
-  function PaddleBorder (key1, key2){ //Reverses the speed of x when x hits y
-    (key2.wid >= key1.xPos) ? key2.speX = -key2.speX :
-    (key2.yPos >= key1.hei) ? key2.speY = -key2.speY : key2.speY =key2.speY
-    (key2.wid >= key1.xPos) ? key2.speX = -key2.speX :
-    (key2.xPos >= key1.wid) ? key2.speY = -key2.speY : key2.speY =key2.speY
-  }
-
-  function SetBorderY (key1){
-    (key1.hei >= $("#board").height()) ? key1.speY = -key1.speY :
-                      (key1.yPos <= 0) ? key1.speY = -key1.speY : key1.speY = key1.speY
-  }
-
-  function SetYBorder (key){
-    if (key.posY === 0) {
-      key.speY = -key.speY
-    }
-    if (key.hei === BOARDHEIGHT) {
-      key.speY = -key.speY
-    }
-    /*
-    if (positionX >= BOARDWIDTH-65) {
-      speedX = -speedX
-    }
-    else if (positionX < 0) {
-      speedX = -speedX
-    }
-
-    if ((square1.rightX > square2.leftX) && (square1.leftX < square2.rightX) &&
-        (square1.topY < square2.bottomY) && (square1.bottomY > square2.topY)){
-      return true;
-    } else {
-      return false
-    }
-    */
-  }
-
-
-
 }
+
 /* function that checks the bounderies of the walls for the ball
     ball doen't bounce but it awards a point to the other player
     ball resests in the middle
